1ae8c9dda30c6a46b43d96950c23cb1f5bfebe4d
mahalakshmi-j <48760409+mahalakshmi-j@users.noreply.github.com>
2022-09-20 18:34:14 +0530

    Add files via upload

---
+++ src/main/java/com/cognizant/sportsEventMicroservice/SportsEventMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.cognizant.sportsEventMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SportsEventMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsEventMicroserviceApplication.class, args);
+	}
+
+}
