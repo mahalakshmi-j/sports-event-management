b0c8922ca77d69cab587b9e2276f727b92993871
mahalakshmi-j <48760409+mahalakshmi-j@users.noreply.github.com>
2022-09-20 17:49:13 +0530

    Delete sportsEventMicroservice directory

--- sportsEventMicroservice/src/main/java/com/cognizant/sportsEventMicroservice/repository/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.sportsEventMicroservice.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.cognizant.sportsEventMicroservice.entity.Event;
-
-public interface EventRepository extends CrudRepository<Event, Long> {
-}
--- sportsEventMicroservice/src/main/java/com/cognizant/sportsEventMicroservice/repository/SportsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.sportsEventMicroservice.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.cognizant.sportsEventMicroservice.entity.Sports;
-
-public interface SportsRepository extends CrudRepository<Sports, Long> {
-
-}
